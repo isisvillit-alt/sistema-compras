58ea492b868959fc3b74b549ce7d1db7f2edd9e6
isisvillit-alt <isisvillit@gmail.com>
2026-06-17 10:47:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+mysql = MySQL(app)
 # =========================
 # UPLOADS
 # =========================
