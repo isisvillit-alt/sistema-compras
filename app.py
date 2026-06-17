86b89626b11a98229215e4d2f8023bf5f28d0eeb
isisvillit-alt <isisvillit@gmail.com>
2026-06-17 12:28:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -497,7 +497,9 @@ def reset_password(token):
 
 # =========================
 # RUN
-# =========================
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
