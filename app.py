d38ed7292437a35df1a31bdf1847ee3f7e4327d3
isisvillit-alt <isisvillit@gmail.com>
2026-06-18 12:48:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import uuid
 from datetime import datetime
 
-from itsdangerous imfrom flask import Flask, render_template, request, redirect, send_file, session, send_from_directoryport URLSafeTimedSerializer
+from flask import Flask, render_template, request, redirect, send_file, session, send_from_directory
+from itsdangerous import URLSafeTimedSerializer
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from flask_mail import Mail, Message
